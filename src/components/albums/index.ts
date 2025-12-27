ab0c9cf126264ca8336ec239dd74b3cd245ee3c1
YuvrajRajbanshi <singhraj0461@gmail.com>
2025-12-27 16:02:31 +0530

    Frontend for uploading images for the album and it's working fine

--- src/components/albums/index.ts
+++ src/components/albums/index.ts
@@ -2,3 +2,4 @@ export { default as MemoryAlbums } from "./MemoryAlbums";
 export { default as AlbumCard } from "./AlbumCard";
 export { default as NewAlbumCard } from "./NewAlbumCard";
 export { default as AddAlbum } from "./AddAlbum";
+export { default as AlbumDetail } from "./AlbumDetail";
